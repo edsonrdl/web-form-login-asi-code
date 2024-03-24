64bc2b53bed9e0ba6afa6cfdb320fee2ee6d262b
edsonrdl <91911044+edsonrdl@users.noreply.github.com>
2024-03-24 18:47:05 -0300

    feat:Finalizado

--- js/login.js
+++ js/login.js
@@ -2,6 +2,9 @@
 const form = document.querySelector('.form');
 const containerMessageText= document.querySelector('.container-cat-asi-code-message');
 
+if(form.event=="submit"){
+
+}
 form.addEventListener('submit', function(event) {
     event.preventDefault();
 
@@ -35,12 +38,35 @@ function displayMessage(message, messageType) {
     p.classList.add("message-text");
 
     div.appendChild(p);
+    setTimeout(() => {
+        div.classList.add('content-text-message-show-add'); 
+    }, 100); 
     containerMessageText.appendChild(div);
 
     if (messageType === 'success') {
         containerMessageText.classList.add("success-message");
+
+        setTimeout(() => {
+            removeMessageSuccesfully();
+        }, 5000);
     } else if (messageType === 'error') {
         containerMessageText.classList.add("error-message");
+        setTimeout(() => {
+            removeMessageSuccesfully();
+        }, 5000);
+    }
+    function removeMessageSuccesfully(){
+        const containerDivText=document.querySelector(".content-text-message");
+        const showMessage=document.querySelector(".content-text-message-show-add");
+        showMessage.classList.remove('content-text-message-show-add'); 
+        showMessage.classList.add('content-text-message-show-remove'); 
+        setTimeout(() => {
+            containerDivText.remove();
+        }, 100); 
+
+
+       
+
     }
 }
 
